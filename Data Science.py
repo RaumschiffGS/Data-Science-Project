9d70aa140f877fd2c014f8a482610878f4089198
RaumschiffGS <nilsnover@gmail.com>
2020-06-12 11:35:14 +0200

    test

---
+++ Data Science.py
@@ -0,0 +1,22 @@
+try:
+    import pandas as pa
+    import numpy as np
+    import math
+except:
+    import pip
+    pip.main(['install','pandas'])
+    import pandas as pa
+    import numpy as np
+    pip.main(['install','numpy'])
+    import math
+    pip.main(['install','math'])
+
+
+#reading in data frpm xlsx file
+NO2_2001 = pa.read_csv('.\NO2 Werte\no2_2001.csv')
+
+
+#functions
+
+
+
