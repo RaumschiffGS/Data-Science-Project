cb4d1fb77c85c5fe80b383fd986c55865afc27c7
RaumschiffGS <nilsnover@gmail.com>
2020-06-12 11:38:07 +0200

    ich kann gar nix lel

--- Data Science.py
+++ Data Science.py
@@ -14,7 +14,7 @@ except:
 
 #reading in data frpm xlsx file
 NO2_2001 = pa.read_csv('.\NO2 Werte\no2_2001.csv')
-
+Kraftfahrzeugbestand = pa.read_csv('.\Kraftfahrzeugbestand\Kraftfahrzeugbestand.csv')
 
 #functions
 
